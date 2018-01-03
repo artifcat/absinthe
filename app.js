2d75e89b30724f172b730c480be62449429789d1
RaysAndLazors <raysandlazors@gmail.com>
2018-01-03 23:41:17 +0100

    Basic user login/logout/registration

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-//Packages
-var express = require('express');
-var multer = require('multer');
-var bodyParser = require('body-parser'); 
-
-var Database = require('./modules/db.js');
-
 //Constants
 const port = 3000;
 const dburl = 'mongodb://localhost:27017/absinthe';
 const imgpath = 'M:/Studia/PJA/TIN/img/';
-const db = new Database(dburl);
+
+//Packages
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+var models = require('./modules/models.js')(mongoose);
+
+//Variables
+var app = express();
+var upload = multer({ storage: Storage });
 
 //Settings
 var Storage = multer.diskStorage({
@@ -19,31 +29,75 @@ var Storage = multer.diskStorage({
     filename: function (req, file, callback) {
         //todo: verify we are uploading pictures
         var namesplit = file.originalname.split('.');
-        callback(null,"img_" + Date.now() + "." + namesplit[namesplit.length-1]);
+        callback(null,"img" + Date.now() + "." + namesplit[namesplit.length-1]);
     }
 });
 
-//Variables
-var app = express();
-var upload = multer({ storage: Storage });
-
-//  test
-
-// /test
-
-//static linking
-app.use('/img', express.static(imgpath));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use(bodyParser.json()); 
+passport.use(models.User.createStrategy());
+passport.serializeUser(models.User.serializeUser());
+passport.deserializeUser(models.User.deserializeUser());
 
 app.set('view engine', 'pug');
 
+app.use('/img', express.static(imgpath));
+app.use('/js', express.static(__dirname + '/public/js'));
+
+
+app.use(cookieParser('catgirls'));
+app.use(session({
+    secret: 'catgirls',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(dburl);
 
 app.get("/", function(req, res){
-    db.getImages([], function(result){
-        res.render('index', { title: 'Hey', message: 'Images', imageArray: JSON.stringify(result)});
+    res.render('index', { title: 'Hey', message: 'Images', user: req.user});
+});
+
+app.get('/flash', function(req, res){
+    req.flash('error', 'Flash is back!')
+    res.redirect('/login');
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+app.get("/login", function(req, res){
+    res.render('login', { error: req.flash('error') });
+});
+
+app.post("/login", passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: 'Invalid username or password.' }), function(req, res){
+    res.redirect('/');
+});
+
+app.get("/register", function(req, res){
+    res.render('register');
+});
+
+app.post("/register", function(req, res){
+    models.User.register({username: req.body.username, email: req.body.email}, req.body.password, function(err, user) {
+        if (err) { 
+            //todo: handle error?
+            console.log(err);
+        }
+        else{
+            req.login(user,function(err) {
+                if (err) { return next(err); }
+                return res.redirect('/');
+            });
+        }
     });
-    
 });
 
 app.post("/add", upload.single('image'), function(req, res, next){
---
+++ modules/models.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+module.exports = function(mongoose) {
+    var User = new Schema({
+        email               :   String,
+    });
+    var Image = new Schema({
+        filename            :   {type: String, index: true},
+        id                  :   ObjectId,
+        thumbnail           :   {data: Buffer, contentType: String}
+    });
+
+    User.plugin(passportLocalMongoose);
+
+    var models = {
+      User  : mongoose.model('Users', User),
+      Image : mongoose.model('Images', Image),
+    };
+
+    //console.log(User);
+
+    return models;
+}
